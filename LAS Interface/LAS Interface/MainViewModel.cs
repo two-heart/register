962fd7957dd42cfb592b549eea17ac3d32d818d5
Timo Borner <timo@borners.de>
2016-09-25 16:58:05 +0200

    Starting Interface Production :)

---
+++ LAS Interface/LAS Interface/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using LAS_Interface.Types;
+
+namespace LAS_Interface
+{
+    public class MainViewModel :INotifyPropertyChanged
+    {
+        private List<DataObject> _dataObjects;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public List<DataObject> DataObjects
+        {
+            get { return _dataObjects; }
+            set
+            {
+                _dataObjects = value;
+                OnPropertyChanged(nameof(DataObjects));
+            }
+        }
+
+
+        protected void OnPropertyChanged (string name)
+        {
+            PropertyChanged?.Invoke (this, new PropertyChangedEventArgs (name));
+        }
+    }
+}
---
+++ LAS Interface/LAS Interface/Types/DataObject.cs
@@ -0,0 +1,12 @@
+﻿namespace LAS_Interface.Types
+{
+    public class DataObject
+    {
+        public string Title;
+
+        public DataObject(string title)
+        {
+            Title = title;
+        }
+    }
+}
